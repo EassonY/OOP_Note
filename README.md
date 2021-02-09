182868d3ab2b43c6a6141a65ea476635ab6e672b
EassonY <46298663+EassonY@users.noreply.github.com>
2021-02-09 22:09:21 +0800

    1. #ifndef  #define  #endif 2. class complex 3. test file

---
+++ README.md
@@ -0,0 +1,3 @@
+Some OOP Notes
+
+Learned from Houjie.
---
+++ comlex_test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "complex.h"
+
+using namespace std;
+
+ostream&
+operator << (ostream& os, const complex& x)
+{
+    return os << '(' << real (x) ',' << imag (x) << ')';
+}
+
+int main()
+{
+    complex c1(2, 1);
+    complex c2(4, 0);
+
+    cout << c1 << endl;
+    cout << c2 << endl;
+
+    cout << c1 + c2 << endl;
+    cout << c1 - c2 << endl;
+    cout << c1 * c2 << endl;
+    cout << c1 /  2 << endl;
+
+
+    return 0;
+}
