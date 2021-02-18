eabe80466a4c013ba864d5122775f037ac2ab006
EassonY <46298663+EassonY@users.noreply.github.com>
2021-02-18 22:54:38 +0800

    finish the string.h

--- string.h
+++ string.h
@@ -31,6 +31,39 @@ String::String(const char* cstr)
     }
 }
 
+inline
+String::~String()
+{
+    delete[] m_data;
+}
 
+inline
+String& String::operator=(const String& str)
+{
+    if (this == &str)
+        return *this;   // 
+    
+    delete[] m_data;
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+    return *this;       //
+}
+
+inline
+String::String(const String& str)
+{
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+}
+
+#include <iostream>
+using namespace std;
+
+ostream& operator<<(ostream& os, const String& str)
+{
+    os << str.get_c_str();
+    return os;
+
+}
 
 #endif
